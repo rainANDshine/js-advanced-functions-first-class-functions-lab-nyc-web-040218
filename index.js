2d055c8980fdcb77314ca679323c64af38134505
Shun Yao <ysmiracle@hotmail.com>
2018-05-15 03:26:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const firstTwoDrivers = function returnFirstTwoDrivers(drivers) {
+const returnFirstTwoDrivers = function(drivers) {
   return [drivers[0], drivers[1]];
 }
 

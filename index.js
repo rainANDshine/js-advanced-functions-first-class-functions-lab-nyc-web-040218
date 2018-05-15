7f0f50bce8ffd5c7134ba952af09a2ae886e22bf
Shun Yao <ysmiracle@hotmail.com>
2018-05-15 03:00:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const firstTwoDrivers = function returnFirstTwoDrivers(drivers) {
+  return [drivers[0], drivers[1]];
+}
+
+const lastTwoDrivers = function returnLastTwoDrivers(drivers) {
+  return [drivers[-2], drivers[-1]];
+}
+
+selectingDrivers = [firstTwoDrivers, lastTwoDrivers];
+
+function createFareMultiplier(num1) {
+  return function(num2) {
+    return num1 * num2;
+  }
+}
+

8e58d5cb4adec9d6dd77bd734052b52cbc863042
Shun Yao <ysmiracle@hotmail.com>
2018-05-15 03:42:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function createFareMultiplier(num1) {
   }
 }
 
-const fareDoubler = function(num) {
-  return num * createFareMultiplier(2);
-}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
